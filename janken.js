f6cc68e10fbe57c4161d6e807a2643a8e40a9a0b
nicknamegrape <kurumeteacher@gmail.com>
2025-10-20 20:36:25 +0900

    add dogs_playing_guess_stationery

--- janken.js
+++ janken.js
@@ -1,51 +1,48 @@
-define(function () {
-	return playJanken;
-	function playJanken(players) {
-		var rflag = false;
-		var sflag = false;
-		var pflag = false;
-		players.forEach(function (el) {
-			if (el.canJanken) {
-				var rand = Math.random();
-				if (rand < 1/3) {
-					el.janken = "rock";
-				} else if (rand < 2/3) {
-					el.janken = "scissors";
-				} else {
-					el.janken = "paper";
-				}
+function playJanken(players) {
+	var rflag = false;
+	var sflag = false;
+	var pflag = false;
+	players.forEach(function (el) {
+		if (el.canJanken) {
+			var rand = Math.random();
+			if (rand < 1/3) {
+				el.janken = "rock";
+			} else if (rand < 2/3) {
+				el.janken = "scissors";
 			} else {
-				el.janken = null;
+				el.janken = "paper";
 			}
-			if (el["janken"] === "rock") {
-				rflag = true;
-			} else if (el["janken"] === "scissors") {
-				sflag = true;
-			} else if (el["janken"] === "paper") {
-				pflag = true;
-			}
-		});
-		var count = 0;
-		players.forEach(function (p) {
-			if (p["canJanken"]) {
-				count += 1;
-			}
-		});
-		if ((rflag + sflag + pflag === 1) ||
-			(rflag && sflag && pflag)) { //draw
-			console.log("draw");
-			return;
+		} else {
+			el.janken = null;
 		}
-		game.players.forEach(function (p) {
-			if (!rflag && p["janken"] === "paper") {
-				p["canJanken"] = false;
-			}
-			if (!sflag && p["janken"] === "rock") {
-				p["canJanken"] = false;
-			}
-			if (!pflag && p["janken"] === "scissors") {
-				p["canJanken"] = false;
-			}
-		});
-	};
-});
+		if (el["janken"] === "rock") {
+			rflag = true;
+		} else if (el["janken"] === "scissors") {
+			sflag = true;
+		} else if (el["janken"] === "paper") {
+			pflag = true;
+		}
+	});
+	var count = 0;
+	players.forEach(function (p) {
+		if (p["canJanken"]) {
+			count += 1;
+		}
+	});
+	if ((rflag + sflag + pflag === 1) ||
+		(rflag && sflag && pflag)) { //draw
+		console.log("draw");
+		return;
+	}
+	players.forEach(function (p) {
+		if (!rflag && p["janken"] === "paper") {
+			p["canJanken"] = false;
+		}
+		if (!sflag && p["janken"] === "rock") {
+			p["canJanken"] = false;
+		}
+		if (!pflag && p["janken"] === "scissors") {
+			p["canJanken"] = false;
+		}
+	});
+};
